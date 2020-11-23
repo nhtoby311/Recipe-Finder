35689d593b9ac71b771e21961df4ce0d77d105ae
Toby <nvminhhieu1999@gmail.com>
2020-11-23 18:40:37 +0100

    Recipe Finder v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react"
 import './App.css';
+import Recipe from './components/Recipe'
 
 function App() {
+
+  
+  //pls dont steal this :(
+  const APP_ID = "c22b125c"                   
+  const APP_KEY = "e908f59031d64f42a5c926642c7296a4"
+
+  
+  const[recipes, setRecipes] = useState([])
+  const[search, setSearch] = useState("pizza")
+  const[query, setQuery] = useState("pizza")
+
+  useEffect(function()
+  {
+    console.log("run1st")
+    getRecipes()
+  },[query])
+
+  const getRecipes = async function()
+  {
+    console.log(APP_ID + " and "+ APP_KEY )
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    
+    setRecipes(data.hits)
+    console.log(data.hits)
+  }
+
+  const updateSearch = function(e)
+  {
+    setSearch(e.target.value)
+    console.log(search)
+  } 
+
+  const getSearch = function(e)
+  {
+    e.preventDefault()
+    setQuery(search)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-name">Recipe Finder</h1>
+      <form onSubmit={getSearch} className="search-form" >
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}></input>
+        <button className="search-button" type="submit">
+        Search
+        </button>
+      </form>
+      <div className="recipe-cont">
+        {recipes.map(function(recipe)
+        { 
+          return <Recipe 
+          key={recipe.recipe.label}
+          name={recipe.recipe.label}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredientLines}
+          />
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Recipe.js
@@ -0,0 +1,22 @@
+function Recipe(props)
+{
+    return(
+        
+        <div className="recipe-container">
+            <div className="recipe-card" style={{backgroundImage: `url(${props.image})`}}>
+                <h1 className="title-recipe">{props.name}</h1>
+            </div>
+            <div className="ingredient-box">
+                    <ul className="list">
+                    {props.ingredients.map(function(ingridient){
+                        return <li className="ingredient">
+                        {ingridient}
+                        </li>
+                    }
+                    )}
+                    </ul>
+            </div>
+        </div>
+    );
+}
+export default Recipe
